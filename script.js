1c9f14265a87e4bb3c13dfa73d63bf203224ba21
Aqib Ali Kalwar <aqibalikalwar1@gmail.com>
2024-02-21 20:01:10 +0500

    Added my data to my Portfolio webdsite

--- script.js
+++ script.js
@@ -34,14 +34,14 @@ $(document).ready(function () {
 
   // typing text animation script
   var typed = new Typed(".typing", {
-    strings: ["Developer", "Designer", "Freelancer"],
+    strings: ["Web Developer", "Web Designer"],
     typeSpeed: 100,
     backSpeed: 60,
     loop: true,
   });
 
   var typed = new Typed(".typing-2", {
-    strings: ["Developer", "Designer", "Freelancer"],
+    strings: ["Web Developer", "Web Designer"],
     typeSpeed: 100,
     backSpeed: 60,
     loop: true,
